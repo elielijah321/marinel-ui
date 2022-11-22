7e5a7787258c88c222ea944b7290bfc4c152b998
elielijah321 <45821029+elielijah321@users.noreply.github.com>
2022-11-22 20:47:37 +0000

    add library book

---
+++ Data/Entities/LibraryBook.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Marinel_ui.Data.Entities
+{
+    public class LibraryBook
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Quantity { get; set; }
+    }
+}
+
+
